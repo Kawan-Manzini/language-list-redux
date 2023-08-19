b724a3d7c2610f3f9a064960b020a6903e7b8440
Kawan Manzini <kawan.manzini@qodeless.io>
2023-08-19 11:40:48 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addLanguages, toFavorite, useLanguages } from "./redux/sliceLanguages";
+import { link } from "fs";
 
 function App() {
+  const languages = useSelector(useLanguages);
+
+  const [newLanguage, setNewLanguage] = useState('')
+
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <ul>
+          {languages.map((language) => {
+            return (
+              <div style={{display: "flex", gap: "20px", marginBottom: "10px"}}>
+                <span style={{color: language.favorite ? 'green' : 'black'}}>{language.name}</span>
+                <button type="button" onClick={() => dispatch(toFavorite(language.name))}>{language.favorite ? 'Desfavoritar' : 'Favoritar'}</button>
+              </div>
+            )
+            })}
+        </ul>
+      </div>
+      <div>
+        <input type="text" value={newLanguage} onChange={(e) => setNewLanguage(e.target.value)} />
+        <button type="button" onClick={() => dispatch(addLanguages(newLanguage))}>Adicionar nova Linguagem</button>
+      </div>
     </div>
   );
 }
---
+++ src/redux/sliceLanguages.tsx
@@ -0,0 +1,38 @@
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+
+export interface Languages{
+  name: string;
+  favorite: boolean;
+
+}
+
+const INITIAL_STATE: Languages[] = [
+  {name: "Java", favorite : false  },
+  {name: "C#", favorite : false  },
+  {name: "JavaScript", favorite : false  },
+  {name: "C++", favorite : false  },
+]
+
+const sliceLanguages = createSlice({
+  name: 'languages',
+  initialState: INITIAL_STATE,
+  reducers: {
+    addLanguages(state, {payload}: PayloadAction<string>){
+        return [...state, {name: payload, favorite: false}]
+    },
+    
+    toFavorite(state, {payload}: PayloadAction<string>){
+      return state.map(st => st.name === payload ? {...st, favorite: !st.favorite} : st)
+    }
+
+
+  }
+
+})
+
+export default sliceLanguages.reducer;
+export const {addLanguages,toFavorite} = sliceLanguages.actions;
+
+export const useLanguages = (state: any) => {
+  return state.languages as Languages[];
+};
